fd9d6cc0268a197af165aea3a2b5c3e9d3a60025
Brian <huynh.sbrian@gmail.com>
2022-06-27 17:51:46 -0700

    commit tree 9a34a5f37299fad70b19c7a25317eb67972fbb9a

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 25;
+const num4 = 7;
+
+const mod = num3 % num4;
+
+var max = Math.max(1, 2, 3, 4, 20);
